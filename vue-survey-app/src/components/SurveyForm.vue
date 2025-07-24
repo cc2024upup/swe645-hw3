391eb036ba080dff76023eb4ead9aaff7aded525
cc2024upup <161077844+cc2024upup@users.noreply.github.com>
2025-07-24 15:24:55 -0400

    Update SurveyForm.vue

--- vue-survey-app/src/components/SurveyForm.vue
+++ vue-survey-app/src/components/SurveyForm.vue
@@ -160,7 +160,7 @@ export default {
         this.setCookie('username', this.form.username);
 
         // sent data to spring boot
-        fetch('http://54.235.178.80:30623/api/surveys', {
+        fetch('api/surveys', {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json'
